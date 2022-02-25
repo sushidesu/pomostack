02ff0c1bff9777ac340a5210da98d20c6fee54ef
sushidesu <hugemerge@gmail.com>
2022-02-25 18:31:21 +0900

    init template

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next"
+import Head from "next/head"
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>PomoStack</title>
+        <meta name="description" content="Stack and record your pomodoros" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default Home
